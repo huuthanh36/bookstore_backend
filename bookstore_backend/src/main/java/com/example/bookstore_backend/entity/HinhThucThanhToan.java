c948d1b0d12f1e5070475624c130c9d68ed93a02
Nguyễn Hữu Thành <121446657+huuthanh36@users.noreply.github.com>
2026-06-15 18:33:16 +0700

    Tao cac class va anh xa cac thuc the sang CSDL (Hibernate JPA)

---
+++ bookstore_backend/src/main/java/com/example/bookstore_backend/entity/HinhThucThanhToan.java
@@ -0,0 +1,32 @@
+package com.example.bookstore_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+@Entity
+@Data
+@Table(name = "hinh_thuc_thanh_toan")
+public class HinhThucThanhToan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ma_hinh_thuc_thanh_toan")
+    private int maHinhThucThanhToan;
+
+    @Column(name = "ten_hinh_thuc_thanh_toan")
+    private String tenHinhThucThanhToan;
+
+    @Column(name = "mo_ta")
+    private String moTa;
+
+    @Column(name = "chi_phi_thanh_toan")
+    private double chiPhiThanhToan;
+
+    @OneToMany(mappedBy = "hinhThucThanhToan", fetch = FetchType.LAZY, cascade = {
+            CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.REFRESH, CascadeType.DETACH
+    })
+    private List<DonHang> danhSachDonHang;
+
+}
---
+++ bookstore_backend/src/main/java/com/example/bookstore_backend/entity/Sach.java
@@ -0,0 +1,73 @@
+package com.example.bookstore_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "sach")
+public class Sach {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ma_sach")
+    private int maSach;
+
+    @Column(name = "ten_sach", length = 256)
+    private String tenSach;
+
+    @Column(name = "ten_tac_gia", length = 256)
+    private String tenTacGia;
+
+    @Column(name = "isbn", length = 256)
+    private String ISBN;
+
+    @Column(name = "mo_ta", columnDefinition = "text")
+    private String moTa;
+
+    @Column(name = "gia_niem_yet")
+    private double giaNiemYet;
+
+    @Column(name = "gia_ban")
+    private double giaBan;
+
+    @Column(name = "so_luong")
+    private int soLuong;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {
+            CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.REFRESH, CascadeType.DETACH
+    })
+    @JoinTable(
+            name = "sach_theloai",
+            joinColumns = @JoinColumn(name = "ma_sach"),
+            inverseJoinColumns = @JoinColumn(name = "ma_the_loai")
+    )
+    List<TheLoai> danhSachTheLoai;
+
+    @OneToMany(mappedBy = "sach", fetch = FetchType.LAZY, cascade = {
+            CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.REFRESH, CascadeType.DETACH,
+            CascadeType.REMOVE
+    })
+    List<HinhAnh> danhSachHinhAnh;
+
+    @OneToMany(mappedBy = "sach", fetch = FetchType.LAZY, cascade = {
+            CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.REFRESH, CascadeType.DETACH
+    })
+    List<SuDanhGia> danhSachDanhGia;
+
+    @OneToMany(mappedBy = "sach", fetch = FetchType.LAZY, cascade = {
+            CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.REFRESH, CascadeType.DETACH
+    })
+    List<ChiTietDonHang> danhSachChiTietDonHang;
+
+    @OneToMany(mappedBy = "sach", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    List<SachYeuThich> danhSachSachYeuThich;
+
+}
